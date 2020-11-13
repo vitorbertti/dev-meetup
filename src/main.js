830aa4397288c2965daa3ba28c6fc133488d9e6a
Vitor Bertti <berttivitor@gmail.com>
2020-11-13 20:51:51 -0300

    Added router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import vuetify from '@/plugins/vuetify';
-import App from './App.vue'
+import App from './App.vue';
+import router from '@/router/router';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   vuetify,
-}).$mount('#app')
+  router,
+}).$mount('#app');
