629084f3b1cef5ad9fea2b4eee66d1e74c97fe83
Vitor Bertti <berttivitor@gmail.com>
2020-11-14 19:59:28 -0300

    Changed to VueRouter and added history mode

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld.vue';
+import VueRouter from 'vue-router';
 
 Vue.use(Router);
 
-export default new Router({
+export default new VueRouter({
+   mode: 'history',
    routes: [
       {
          path: '/hello',
