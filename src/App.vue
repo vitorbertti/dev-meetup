f79165722ab7fd90a751ca920cd4100a0c9e3552
Vitor Bertti <berttivitor@gmail.com>
2020-11-16 19:44:46 -0300

    Initialized global styles

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,17 @@
           <v-list-item-action>
             <v-icon>mdi-account-supervisor</v-icon>
           </v-list-item-action>
+          <v-list-item-content>View Meetups</v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar>
-      <v-app-bar-nav-icon @click.native.stop="sideNav = !sideNav"></v-app-bar-nav-icon>
+    <v-toolbar dark class="primary">
+      <v-app-bar-nav-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon>
       <v-toolbar-title>DevMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items class="d-none d-sm-flex">
         <v-btn text>
-          <v-icon left>mdi-account-supervisor</v-icon>
+          <v-icon left dark>mdi-account-supervisor</v-icon>
           View Meetups
         </v-btn>
       </v-toolbar-items>
@@ -39,5 +40,5 @@ export default {
 </script>
 
 <style>
-
+@import url('./styles/global.scss');
 </style>
