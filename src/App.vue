1394e137b6df17e53db79153dc3a17eb49ce8689
Vitor Bertti <berttivitor@gmail.com>
2020-11-19 22:00:43 -0300

    Added app links

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="sideNav">
+    <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
-        <v-list-item v-for="item in menuItems" :key="item.title">
+        <v-list-item v-for="item in menuItems" :key="item.title" router :to="item.link">
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -12,10 +12,12 @@
     </v-navigation-drawer>
     <v-toolbar dark class="primary">
       <v-app-bar-nav-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon>
-      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to='/' tag="span" style="cursor:pointer;">DevMeetup</router-link> 
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="d-none d-sm-flex">
-        <v-btn text v-for="item in menuItems" :key="item.title">
+        <v-btn text v-for="item in menuItems" :key="item.title" router :to="item.link">
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
@@ -38,22 +40,27 @@ export default {
         {
           icon: 'mdi-account-supervisor',
           title: 'View Meetups',
+          link: '/meetups',
         },
         {
           icon: 'mdi-room',
           title: 'Organize Meetups',
+          link: '/createMeetup',
         },
         {
           icon: 'mdi-person',
           title: 'Profile',
+          link: '/profile',
         },
         {
           icon: 'mdi-face',
           title: 'Sign up',
+          link: '/signin',
         },
         {
           icon: 'mdi-lock-open',
           title: 'Sign in',
+          link: '/signup',
         },
       ]
     }
