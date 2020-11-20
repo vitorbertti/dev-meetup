4555f012b4cac7ff1a7363de1fff3f8447eb960b
Vitor Bertti <berttivitor@gmail.com>
2020-11-20 19:21:47 -0300

    Added carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,52 @@
 <template>
-  <div>
-     <h1>The Home Page</h1>
-  </div>
+  <v-container>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="primary">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/createMeetups" class="primary">Organize Meetups</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item v-for="meetup in meetups" :src="meetup.imageUrl" :key="meetup.id">
+            <div class="title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      meetups: [
+        {imageUrl: '', id: '12312435345', title: 'Meetup in New York'},
+        {imageUrl: '', id: '23423444343', title: 'Meetup in Paris'},
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background: rgba(0, 0, 0, 0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
 </style>
