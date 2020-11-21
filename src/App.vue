bda0779fc692b49454a772e073903b5763cc2aa8
Vitor Bertti <berttivitor@gmail.com>
2020-11-21 20:28:37 -0300

    Added meetups page v-card

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <v-navigation-drawer temporary v-model="sideNav">
       <v-list>
-        <v-list-item v-for="item in menuItems" :key="item.title" router :to="item.link">
+        <v-list-item v-for="item in menuItems" :key="item.title" :to="item.link">
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -11,13 +11,13 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar dark class="primary">
-      <v-app-bar-nav-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon>
       <v-toolbar-title>
         <router-link to='/' tag="span" style="cursor:pointer;">DevMeetup</router-link> 
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="d-none d-sm-flex">
-        <v-btn text v-for="item in menuItems" :key="item.title" router :to="item.link">
+        <v-btn text v-for="item in menuItems" :key="item.title" :to="item.link">
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>
-     <h1>The Meetups Page</h1>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card class="info">
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs5 sm4 md3>
+                <v-card-media src="" height="130px" ></v-card-media>
+              </v-flex>
+              <v-flex xs7 sm8 md9>
+                <v-card-title primary-title>
+                  <div>
+                    <h5 class="white--text mb-0">My Meetup</h5>
+                    <div>17th July 2017</div>
+                  </div>
+                </v-card-title>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
