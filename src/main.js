bf2fc6db7b7be7bf972a4c10c819be32abf1c04d
Vitor Bertti <berttivitor@gmail.com>
2020-11-28 20:11:14 -0300

    Added firebase

--- src/main.js
+++ src/main.js
@@ -4,10 +4,21 @@ import App from './App.vue';
 import router from '@/router/router';
 import { store } from './store';
 import DateFilter from './filters/date';
+import * as firebase from 'firebase';
 
 Vue.config.productionTip = false;
 
 Vue.filter('date', DateFilter);
+
+const config = {
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASEURL,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGESENDERID,
+};
+firebase.initializeApp(config);
 
 new Vue({
   render: h => h(App),
