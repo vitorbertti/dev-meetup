45b4436953ded34d04b38532acbb44cfe79607f8
Vitor Bertti <berttivitor@gmail.com>
2020-11-27 21:22:39 -0300

    Added date filter

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import vuetify from '@/plugins/vuetify';
 import App from './App.vue';
 import router from '@/router/router';
-import { store } from './store'
+import { store } from './store';
+import DateFilter from './filters/date';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.filter('date', DateFilter);
 
 new Vue({
   render: h => h(App),
