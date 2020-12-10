cdf04ab527add6b7b93bc7c5cbcf2d8b9ac61ffe
Vitor Bertti <berttivitor@gmail.com>
2020-12-09 21:48:31 -0300

    Added edit meetup dialog

--- src/components/Meetup/Edit/EditMeetupDetailsDialog.vue
+++ src/components/Meetup/Edit/EditMeetupDetailsDialog.vue
@@ -1,14 +1,47 @@
 <template>
   <v-dialog width="350px" persistent>
-     <v-btn floating accent slot="activator">
+     <v-btn fab accent slot="activator">
         <v-icon>edit</v-icon>
      </v-btn>
+     <v-card>
+        <v-container>
+           <v-layout row wrap>
+              <v-flex xs12>
+                 <v-card-title>Edit Meetup</v-card-title>
+              </v-flex>
+           </v-layout>
+           <v-divider></v-divider>
+           <v-layout row wrap>
+              <v-flex xs12>
+                 <v-card-text>
+                    <v-text-field name="title" label="Title" id="title" required v-model="editedTitle"></v-text-field>
+                    <v-text-field name="description" label="Description" id="description" multi-line required v-model="editedDescription"></v-text-field>
+                 </v-card-text>
+              </v-flex>
+           </v-layout>
+           <v-divider></v-divider>
+           <v-layout row wrap>
+              <v-flex xs12>
+                 <v-card-actions>
+                    <v-btn flat class="blue--text darken-1">Close</v-btn>
+                    <v-btn flat class="blue--text darken-1">Save</v-btn>
+                 </v-card-actions>
+              </v-flex>
+           </v-layout>
+        </v-container>
+     </v-card>
   </v-dialog>
 </template>
 
 <script>
 export default {
-
+   props: ['meetup'],
+   data() {
+      return {
+         editedTitle: this.meetup.title,
+         editedDescription: this.meetup.description,
+      }
+   }
 }
 </script>
 
