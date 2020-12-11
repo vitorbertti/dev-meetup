7191e1dc74dae4fbd34ed2b8153ab8cb0e199398
Vitor Bertti <berttivitor@gmail.com>
2020-12-10 22:05:58 -0300

    Added updateMeetup action and mutation

--- src/components/Meetup/Edit/EditMeetupDetailsDialog.vue
+++ src/components/Meetup/Edit/EditMeetupDetailsDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog width="350px" persistent>
+  <v-dialog width="350px" persistent v-model="editDialog">
      <v-btn fab accent slot="activator">
         <v-icon>edit</v-icon>
      </v-btn>
@@ -23,8 +23,8 @@
            <v-layout row wrap>
               <v-flex xs12>
                  <v-card-actions>
-                    <v-btn flat class="blue--text darken-1">Close</v-btn>
-                    <v-btn flat class="blue--text darken-1">Save</v-btn>
+                    <v-btn flat class="blue--text darken-1" @click="editDialog = false">Close</v-btn>
+                    <v-btn flat class="blue--text darken-1" @click="onSaveChanges">Save</v-btn>
                  </v-card-actions>
               </v-flex>
            </v-layout>
@@ -40,6 +40,20 @@ export default {
       return {
          editedTitle: this.meetup.title,
          editedDescription: this.meetup.description,
+         editDialog: false,
+      }
+   },
+   methods: {
+      onSaveChanges() {
+         if(this.editedTitle.trim() === '' || this.editedDescription.trim() !== '') {
+            return;
+         }
+         this.editDialog = false;
+         this.$store.dispatch('updateMeetupData', {
+            id: this.meetup.id,
+            title: this.editedTitle,
+            description: this.editedDescription,
+         })
       }
    }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -18,6 +18,18 @@ export const store = new Vuex.Store({
       createMeetup(state, payload) {
          state.loadedMeetups.push(payload);
       },
+      updateMeetup(state, payload) {
+         const meetup = state.loadedMeetups.find(meetup => meetup.id === payload.id);
+         if(payload.title) {
+            meetup.title = payload.title;
+         }
+         if(payload.description) {
+            meetup.description = payload.description;
+         }
+         if(payload.date) {
+            meetup.date = payload.date;
+         }
+      },
       setUser(state, payload) {
          state.user = payload;
       },
@@ -85,6 +97,26 @@ export const store = new Vuex.Store({
             console.log(error);
          })
          
+      },
+      updateMeetupData({commit}, payload) {
+         commit('setLoading', true);
+         const updateObj = {};
+         if(payload.title) {
+            updateObj.title = payload.title;
+         }
+         if(payload.description) {
+            updateObj.description = payload.description;
+         }
+         if(payload.date) {
+            updateObj.date = payload.date;
+         }
+         firebase.database().ref('meetups').child(payload.id).update(updateObj).then(() => {
+            commit('setLoading', false);
+            commit('updateMeetup', payload);
+         }).catch(error => {
+            console.log(error);
+            commit('setLoading', false);
+         })
       },
       signUserUp({ commit }, payload) {
          commit('setLoading', true);
